06c3a4e007983073035042834adbdcf40cc90773
Marco Meza <marco.meza_eisei@banorte.com>
2018-10-09 12:40:00 -0500

    Add sign in email component

--- src/app/email/email.component.ts
+++ src/app/email/email.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireAuth } from 'angularfire2/auth';
 // import { AngularFire, AuthProviders, AuthMethods } from 'firebase/firebase-auth';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
+import firebase from 'firebase/firebase';
+
 import { moveIn, fallIn } from '../router.animations';
 
 @Component({
@@ -11,40 +16,36 @@ import { moveIn, fallIn } from '../router.animations';
   host: {'[@moveIn]': ''}
 })
 export class EmailComponent implements OnInit {
-  // state: string = '';
-  // error: any;
+  state: string = '';
+  error: any;
+
+  constructor(public af: AngularFireAuth, private router: Router) {
+    // this.af.auth.onAuthStateChanged((user) => {
+    //     if (user != null) {
+    //       this.router.navigateByUrl('/members');
+    //     } else {
+    //         // User is not logged in, redirect to where you need to.
+    //     }
+    // });
+    }
   //
-  // constructor(public af: AngularFire,private router: Router) {
-  //   this.af.auth.subscribe(auth => {
-  //       if(auth) {
-  //         this.router.navigateByUrl('/members');
-  //       }
-  //     });
-  //   }
-  //
-  // onSubmit(formData) {
-  //   if(formData.valid) {
-  //     console.log(formData.value);
-  //     this.af.auth.login({
-  //       email: formData.value.email,
-  //       password: formData.value.password
-  //     },
-  //     {
-  //       provider: AuthProviders.Password,
-  //       method: AuthMethods.Password,
-  //     }).then(
-  //       (success) => {
-  //       console.log(success);
-  //       this.router.navigate(['/members']);
-  //     }).catch(
-  //       (err) => {
-  //       console.log(err);
-  //       this.error = err;
-  //     })
-  //   }
-  // }
-  //
-  //
+  onSubmit(formData) {
+    if(formData.valid) {
+      console.log(formData.value);
+      this.af.auth.signInWithEmailAndPassword(formData.value.email, formData.value.password)
+      .then(
+        (success) => {
+        console.log(success);
+        this.router.navigate(['/members']);
+      }).catch(
+        (err) => {
+        console.log(err);
+        this.error = err;
+      })
+    }
+  }
+
+
   ngOnInit() {
   }
 
