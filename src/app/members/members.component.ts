acb7d01a504d21ad4ee1375ffb6495fc975604c4
Marco Meza <marco.meza_eisei@banorte.com>
2018-10-09 11:42:46 -0500

    Add members component when user sign up or login

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireAuth } from 'angularfire2/auth';
 // import { AngularFire, AuthProviders, AuthMethods } from 'firebase/firebase-auth';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
+import firebase from 'firebase/firebase';
+
 import { moveIn, fallIn, moveInLeft } from '../router.animations';
 
 @Component({
@@ -11,22 +16,25 @@ import { moveIn, fallIn, moveInLeft } from '../router.animations';
   host: {'[@moveIn]': ''}
 })
 export class MembersComponent implements OnInit {
-  // name: any;
-  // state: string = '';
-  //
-  // constructor(public af: AngularFire,private router: Router) {
-  //   this.af.auth.subscribe(auth => {
-  //       if(auth) {
-  //         this.name = auth;
-  //       }
-  //     });
-  //   }
-  //
-  // logout() {
-  //    this.af.auth.logout();
-  //    console.log('logged out');
-  //    this.router.navigateByUrl('/login');
-  // }
+  name: any;
+  state: string = '';
+
+  constructor(public af: AngularFireAuth, private router: Router) {
+    this.af.auth.onAuthStateChanged((user) => {
+        if (user != null) {
+          this.name = user;
+
+        } else {
+            // User is not logged in, redirect to where you need to.
+        }
+    });
+    }
+
+  logout() {
+     this.af.auth.signOut();
+     console.log('logged out');
+     this.router.navigateByUrl('/login');
+  }
 
   ngOnInit() {
   }
