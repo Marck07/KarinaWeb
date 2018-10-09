a52d369f3ca64f8e183dfe2619804f56ff4aba15
Marco Meza <marco.meza_eisei@banorte.com>
2018-10-09 12:12:59 -0500

    Fix route members

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,14 +7,16 @@ import { LoginComponent } from './login/login.component';
 // import { AuthGuard } from './auth.service';
 import { SignupComponent } from './signup/signup.component';
 import { EmailComponent } from './email/email.component';
+import { MembersComponent } from './members/members.component';
+
 
 export const router: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
     { path: 'signup', component: SignupComponent },
     { path: 'login-email', component: EmailComponent },
-    { path: 'members', component: LoginComponent }
-    // TODO:  ACTIVAR Cuando funcione auth service  
+    { path: 'members', component: MembersComponent }
+    // TODO:  ACTIVAR Cuando funcione auth service
     // { path: 'members', component: LoginComponent, canActivate: [AuthGuard] }
 
 ]
